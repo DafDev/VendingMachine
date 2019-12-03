7e3cade83aaee791baacbe136a858c769a28e681
Daf <afahd.ali@gmail.com>
2019-12-03 08:20:00 +0100

    Added Enumeration class and had CoinDenomination inherit from it
    Enumeration is an Enumeration class which implements IComparable. Moreover I also added the coin denomination which range from 0.01 euro to 1 euro. Next step will be to test and implement interfaces.

--- src/DafCompany.VendingMachine.App/Enumerations/CoinDenomination.cs
+++ src/DafCompany.VendingMachine.App/Enumerations/CoinDenomination.cs
@@ -1,7 +1,13 @@
 namespace DafCompany.VendingMachine.App.Enumerations
 {
-    public class CoinDenomination 
+    public class CoinDenomination : Enumeration
     {
-        
+        public static readonly CoinDenomination Cent1 = new CoinDenomination("0,01 euro",0);
+        public static readonly CoinDenomination Cent2 = new CoinDenomination("0,02 euro",0);
+        public static readonly CoinDenomination Cent5 = new CoinDenomination("0,05 euro",0);
+        public static readonly CoinDenomination Cent10 = new CoinDenomination("0,1 euro",0);
+        public CoinDenomination(string name, int id) : base(name, id)
+        {
+        }
     }
 }
